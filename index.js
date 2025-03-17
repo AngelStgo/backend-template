1e20cafc758726e1ca160ca457ab6e43778ec837
AngelStgo <euangel.stgo@gmail.com>
2025-03-17 11:11:33 -0400

    middleware

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+dotenv.config();
+console.log(process.env.MONGODB_URI);
+
+
 
 
 const PORT = process.env.PORT || 4000;
@@ -11,9 +21,17 @@ app.set('views', "./views");
 app.set("view engine", "pug") // will be stored here
 
 
-// Middlewares
-app.use(express.static("./public"));
 
+// Middlewares
+app.use(express.static("./public")); 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(helmet()); // for hide and secure 
+
+
+
+// Routes 
 app.get('/', (req, res) => {
     res.render("index");
 })
